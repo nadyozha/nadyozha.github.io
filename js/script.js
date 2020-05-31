0813978aac57371759f01d6ddffa491d2cd507a9
Nadyozha <Nadyozha@MacBook-Pro-Nadyozha.local>
2020-05-31 17:19:03 +0300

    marco 1
    1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+	$('.open_menu').click(function(){
+	$('.btn-open').toggleClass('active');
+	$('#menu').toggleClass('active_menu');
+	$('.toggle-none').toggleClass('display_none');
+	$('body').toggleClass('lock');
+});
