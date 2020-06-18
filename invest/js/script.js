68b4c779862e03020b78f19318c6231e7f3d4ee5
Nadyozha <Nadyozha@MacBook-Pro-Nadyozha.local>
2020-06-18 10:08:34 +0300

    video

--- invest/js/script.js
+++ invest/js/script.js
@@ -17,18 +17,15 @@ $(function() {
 	
 	$('#sandwich').click(function() {
 		$('body').toggleClass('menubar-in');
-
 	});
 
 	$('.menu-close').click(function() {
 		$('.menubar-in').removeClass('menubar-in');
-
 	});
 
 	Waves.attach('.btn-orange', ['waves-button', 'waves-float']);
 	Waves.attach('.btn-white', ['waves-button', 'waves-float']);
 	Waves.init()
-});
 
 
     $owlMain = $('.owl-main');
@@ -40,15 +37,12 @@ $(function() {
                 0: {
                     items: 1,
                 },
-                588: {
+                576: {
                     items: 2,
                 },
-                768: {
+                992: {
                     items: 3,
                 },
-                //991: {
-                    //items: 3,
-                //},
             },
 
             dots: false,
@@ -62,39 +56,41 @@ $(function() {
             $owlMain.find(".owl-stage-outer").children().unwrap();
         }
     }
-$(window).resize(initializeMain);
-initializeMain();
+    $(window).resize(initializeMain);
+    initializeMain();
 
-$owlBlog = $('.owl-blog');
-    function initializeBlog() {
-      if (window.matchMedia('(max-width: 1199.98px)').matches) {
-        $owlBlog.addClass('owl-carousel');
-        $owlBlog.owlCarousel({
-            responsive: {
-                0: {
-                    items: 1,
-                },
-                588: {
-                    items: 1,
-                },
-                768: {
-                    items: 2,
-                },
-                991: {
-                    items: 3,
-                },
-            },
+    // $owlBlog = $('.owl-blog');
+    // function initializeBlog() {
+    //   if (window.matchMedia('(max-width: 1199.98px)').matches) {
+    //     $owlBlog.addClass('owl-carousel');
+    //     $owlBlog.owlCarousel({
+    //         responsive: {
+    //             0: {
+    //                 items: 1,
+    //             },
+    //             588: {
+    //                 items: 1,
+    //             },
+    //             768: {
+    //                 items: 2,
+    //             },
+    //             991: {
+    //                 items: 3,
+    //             },
+    //         },
 
-            dots: false,
-            smartSpeed: 1000,
-            stagePadding: 30,
+    //         dots: false,
+    //         smartSpeed: 1000,
+    //         stagePadding: 30,
 
-        });
+    //     });
         
-        } else {
-            $owlBlog.trigger("destroy.owl.carousel").removeClass("owl-carousel owl-loaded");
-            $owlBlog.find(".owl-stage-outer").children().unwrap();
-        }
-    }
-$(window).resize(initializeBlog);
-initializeBlog();
+    //     } else {
+    //         $owlBlog.trigger("destroy.owl.carousel").removeClass("owl-carousel owl-loaded");
+    //         $owlBlog.find(".owl-stage-outer").children().unwrap();
+    //     }
+    // }
+    // $(window).resize(initializeBlog);
+    // initializeBlog();
+
+});
