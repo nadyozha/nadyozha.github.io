5655dfcb86b0beb4200bd65c34db9095ca1107d0
Nadyozha <Nadyozha@MacBook-Pro-Nadyozha.local>
2020-12-04 21:32:30 +0300

    copy
    copy

--- js/script.js
+++ js/script.js
@@ -35,6 +35,27 @@ $(document).ready(function(){
   });
   //подключение плагина для стилизации scroll
   //$(".").mCustomScrollbar();
+  //click to copy svg
+  $(function() {
+    // copy content to clipboard
+    function copyToClipboard(element) {
+      var $temp = $("<input>");
+      $("body").append($temp);
+      $temp.val($(element).text()).select();
+      document.execCommand("copy");
+      $temp.remove();
+    }
+    // copy coupone code to clipboard
+    $('.copy-btn').on("click", function() {
+      var $brat = $(this).siblings(".copy-text");
+      copyToClipboard($brat);
+    });
+  });
+  
+
+
+
+  
 
 
  
