57ea2af4d582909f63032d623c632280f18d42e2
Nadyozha <Nadyozha@MacBook-Pro-Nadyozha.local>
2020-12-22 16:21:08 +0300

    all

--- js/script.js
+++ js/script.js
@@ -18,15 +18,15 @@ $(document).ready(function(){
   });
   //cart active index-html
   $('#cart-plan-2-wrap').click(function() {
-    $('.coins-svg').addClass('coins-none');
-    $('.spanp-with-our span').toggleClass('dn-span');
-    $('.spanp-with-our h1').addClass('db-span');
+    //$('.coins-svg').addClass('coins-none');
+    //$('.spanp-with-our span').toggleClass('dn-span');
+    //$('.spanp-with-our h1').addClass('db-span');
   });
   $('#cart-plan-1-wrap').click(function() {
-    $('.coins-svg').addClass('coins-none');
-    $('.spanp-with-our h1').addClass('db-span');
-    $('.spanp-with-our span').removeClass('dn-span');
-    $('.logo-small-mobil').addClass('db-span');
+    //$('.coins-svg').addClass('coins-none');
+    //$('.spanp-with-our h1').addClass('db-span');
+    //$('.spanp-with-our span').removeClass('dn-span');
+    //$('.logo-small-mobil').addClass('db-span');
   });
   //checkbox registration page
   $('#checkbox_1').on('change', function() {
